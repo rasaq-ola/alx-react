99408af1dca7313875622787af488267ed6c29e8
rasaq-ola <rasaqola196@gmail.com>
2024-09-05 02:18:27 +0100

    adding css and images

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+$('body').prepend('<div id="logo"></div>');
+// Rest of the code from task 1...
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    mode: 'production'
+};
